e9cad44c04e0239a8fddebf4e5e51b5d1d51ccd5
Cameron Reikes <cameronreikes@gmail.com>
2018-02-18 00:11:45 -0800

    Add enemies and spawners

--- scenes/game/game.gd
+++ scenes/game/game.gd
@@ -3,6 +3,9 @@ extends Node2D
 # The left border for the player
 onready var leftBorderShape = get_node("borderLeft/borderLeftCollider").get_shape()
 onready var rightBorderShape = get_node("borderRight/borderRightCollider").get_shape()
+# Enemy ( for debugging )
+onready var enemy = get_node("enemy")
+
 
 func _ready():
 	# Connect the resize window signal to the resize function
@@ -13,10 +16,10 @@ func _ready():
 # Called whenever the window is resized
 func resize():
 	# Set the collider to the bottom of the screen
-#	leftBorderShape.set_a(Vector2())
-#	leftBorderShape.set_b(Vector2(0, OS.get_window_size().y))
-#	print("Setting left border a to ", leftBorderShape.get_a())
-#	print("Setting left border b to ", leftBorderShape.get_b())
+	leftBorderShape.set_a(Vector2())
+	leftBorderShape.set_b(Vector2(0, OS.get_window_size().y))
+	print("Setting left border a to ", leftBorderShape.get_a())
+	print("Setting left border b to ", leftBorderShape.get_b())
 	# Set the right segment to the right side of the screen
 	rightBorderShape.set_a(Vector2(OS.get_window_size().x,0))
 	rightBorderShape.set_b(Vector2(OS.get_window_size().x, OS.get_window_size().y))
